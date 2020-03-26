9b9c561622c2ec1c50a416ddc110cce6cbc47a2d
Arpan Sahoo <arpansahooedu@gmail.com>
2020-03-26 16:34:26 -0400

    Removed folder

--- jhu-covid19bot-web/README.md
+++
@@ -1,2 +0,0 @@
-User interface (Vue.js) for the JHU COVID-19 Q&A Chatbot built with Dialogflow. 
-To learn more about the chatbot, go [here](https://jhu-covid-qa.github.io/).
--- jhu-covid19bot-web/src/Config/index.js
+++
@@ -1,7 +0,0 @@
-export default {
-    gateway: 'https://covid-19-v-02-susjcd.core.ushaflow.io', // <- enter your gateway URL here
-    muted: false, // <- mute microphone at start
-    start_suggestions: ["What is the coronavirus?", "What is social distancing?", "How can I protect myself and my family from coronavirus?"], // <- array of suggestions, displayed at the start screen
-    fallback_lang: 'en', // <- fallback language code, if history mode or network is unavailable
-    voice: 'native' // <- voice of Text-To-Speech; for reference, see: https://developer.mozilla.org/en-US/docs/Web/API/SpeechSynthesisVoice/voiceURI
-}
